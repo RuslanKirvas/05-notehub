598827c31da0d071a3a2504961ba3de54dcfd119
Ruslan Kirvas <kirvas.ruslan@gmail.com>
2026-03-20 23:29:45 +0100

    hw-5notehub-fix2

--- src/services/noteService.ts
+++ src/services/noteService.ts
@@ -2,7 +2,6 @@ import axios from "axios";
 import type { Note } from "../types/note";
 
 const API_BASE_URL = "https://notehub-public.goit.study/api";
-
 const TOKEN = import.meta.env.VITE_NOTEHUB_TOKEN;
 
 const api = axios.create({
@@ -15,9 +14,6 @@ const api = axios.create({
 
 export interface NotesResponse {
   notes: Note[];
-  total: number;
-  page: number;
-  perPage: number;
   totalPages: number;
 }
 
